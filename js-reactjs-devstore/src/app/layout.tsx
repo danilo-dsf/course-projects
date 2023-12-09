f502e6a4b14c48a8f4f8653c1c167963fa6ec481
Danilo Ferreira <danilo1705@gmail.com>
2023-12-09 19:02:36 -0300

    project: js-reactjs-devstore

---
+++ js-reactjs-devstore/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { env } from '@/env'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | devstore',
+    default: 'devstore',
+  },
+  metadataBase: new URL(env.APP_URL),
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt" className={inter.variable}>
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
+    </html>
+  )
+}
